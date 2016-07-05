9db6ea6b6b6592bc355f7c62d3a088aa54157825
grstathis <grstathis@gmail.com>
2016-07-05 13:50:54 +0300

    sdg

--- scripts/sc5.py
+++ scripts/sc5.py
@@ -1,21 +1,46 @@
+from __future__ import print_function
 import subprocess
 import os
 import sys
 import matplotlib
 
+from ipywidgets import interact, interactive, fixed
+import ipywidgets as widgets
+
 HIVE='localhost:10000'
 
 PILOT_PATH='/home/stathis/bde-climate-1'
 
+def run_shell_command(s):
+
+    c = s.split('|')
+    cp = None # the current process
+
+    for i in range(0, len(c)):
+        c[i] = c[i].replace('**PIPE**', '|')
+        cin = cout = None  
+
+        if i > 0: 
+            cin = cp.stdout
+        if i < len(c)-1: 
+            cout = subprocess.PIPE
+
+        sys.stdout.flush()
+        cp = subprocess.Popen(c[i], stdout=cout, stdin=cin, shell=True)
+
+    cp.wait() 
+
+
 def help():
-    print 'Hello, world'
+    print('Hello, world')
 
 def _create_user_structure():
-    ''' make create-structure'''
+   	''' make create-structure'''
+	run_shell_command("make -C /home/stathis/bde-climate-1 create-structure")
     
 def ingest(filename):
     ''' make ingest-file NETCDFFILE=yourfilewithfullpath ''' 
-    print 'Ingesting', filename
+    print('Ingesting', filename)
 
 def export(filename):
     ''' make export-file NETCDFKEY=yourkeysearch NETCDFOUT=nameofnetcdfoutfile '''
@@ -34,5 +59,5 @@ def _run_wrf_nest():
 
 def view_data(data=0):
     ''' matplotlib thingy '''
-    print 'a'
+    print('a')
     pass
