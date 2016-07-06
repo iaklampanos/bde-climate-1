4520a46a51ffaa00daaff37ff585cb9ffd7a117a
grstathis <grstathis@gmail.com>
2016-07-06 21:49:37 +0300

    sc5 dev

--- scripts/sc5.py
+++ scripts/sc5.py
@@ -1,17 +1,24 @@
-from __future__ import print_function
-from subprocess import Popen, PIPE, STDOUT
+#from __future__ import print_function
 import os
 import sys
 import matplotlib
 
-
 from ipywidgets import interact, interactive, fixed
 import ipywidgets as widgets
+from IPython.display import display
+
+import subprocess
+import sys
+from subprocess import Popen, PIPE, STDOUT
 
 HIVE='localhost:10000'
-
 PILOT_PATH = '/home/stathis/bde-climate-1'
 NC_FILENAME = '/home/stathis/sc5/rsdscs_Amon_HadGEM2-ES_rcp26_r2i1p1_200512-203011.nc'
+
+CLUSTER_USER = 'stathis'
+CLUSTER_IP = '172.17.20.106'
+CLUSTER_DATA_DIR = '/home/stathis/Downloads'
+CLUSTER_BUILD_DIR = '/home/stathis/Develop'
 
 def run_shell_command(s):
     c = s.split('|')
@@ -41,17 +48,157 @@ def _create_user_structure():
     ''' make create-structure'''
     run_shell_command("make -C /home/stathis/bde-climate-1 create-structure")
     
-def ingest_st(filename):
-    '''test ingest'''
-    os.popen("make -C /home/stathis/bde-climate-1 ingest-file NETCDFFILE=test")
+def updateIngestHTML(x):
+    txarea.value = x
 
 def ingest(filename):
     ''' make ingest-file NETCDFFILE=yourfilewithfullpath '''
-    print('Ingesting', filename)
-    # s = run_shell_command("make -C /home/stathis/bde-climate-1 ingest-file NETCDFFILE=" + filename)
-    s = run_shell_command('ls')
-    print(s)
+    # print 'Ingesting', filename
+    s = execu(ingest_command(netcdffile=filename))#, pattern="LOAD", fn=updateIngestHTML)
+
+def execu(command, pattern=None, fn=sys.stdout.write):
+    """
+    pattern is not really useful;
+    fn is being called multiple times until the command has completed
+    """
+    p = subprocess.Popen(str(command), shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+    while True:
+        l = p.stdout.readline()
+        if pattern == None:
+            fn(l)
+        else:
+            if str(l).find(pattern) > 0:
+                fn(l)
+        if p.poll() != None:
+            break
+
+def ingest_command(clusteruser=CLUSTER_USER,
+                   clusterip=CLUSTER_IP,
+                   clim1_dd=CLUSTER_DATA_DIR,
+                   clim1_bd=CLUSTER_BUILD_DIR,
+                   netcdffile='somefile'):
+    curr_user = os.environ['USER']
+    sti0 = 'scp ./__FILE__ __UNAME__@__HOST__:__BUILD_DIR__/bde-climate-1'
+    sti1 = 'ssh __UNAME__@__HOST__ -T "export CLIMATE1_CASSANDRA_DATA_DIR=__DATA_DIR__ && export CLIMATE1_BUILD_DIR=__BUILD_DIR__ && cd __BUILD_DIR__/bde-climate-1 && make -s ingest-file NETCDFFILE=__FILE__ CUSER=__CUSER__"'
+    sti = sti0 + ' && ' + sti1
+    sti = sti.replace('__UNAME__', clusteruser)
+    sti = sti.replace( '__HOST__', clusterip)
+    sti = sti.replace('__DATA_DIR__', clim1_dd)
+    sti = sti.replace('__BUILD_DIR__', clim1_bd)
+    sti = sti.replace('__FILE__', netcdffile)
+    sti = sti.replace('__CUSER__', curr_user)
+    # print(sti)
+    return sti
+
+
+def export_command(clusteruser=CLUSTER_USER,
+                   clusterip=CLUSTER_IP,
+                   clim1_dd=CLUSTER_DATA_DIR,
+                   clim1_bd=CLUSTER_BUILD_DIR,
+                   netcdfkey='somekey'):
+    # make export-file NETCDFKEY=yourkeysearch NETCDFOUT=nameofnetcdfoutfile
+    sti0 = 'echo "Retrieving __FILE__..." && scp __UNAME__@__HOST__:__BUILD_DIR__/bde-climate-1/__FILE__ .'
+    sti1 = 'echo "Exporting __FILE__..." && ssh  __UNAME__@__HOST__ -T "export CLIMATE1_CASSANDRA_DATA_DIR=__DATA_DIR__ && export CLIMATE1_BUILD_DIR=__BUILD_DIR__ && cd __BUILD_DIR__/bde-climate-1 && make -s export-file NETCDFKEY=__KEY__ NETCDFOUT=__FILE__"'
+    sti = sti1 + ' && ' + sti0
+    sti = sti.replace('__UNAME__', clusteruser)
+    sti = sti.replace( '__HOST__', clusterip)
+    sti = sti.replace('__DATA_DIR__', clim1_dd)
+    sti = sti.replace('__BUILD_DIR__', clim1_bd)
+    sti = sti.replace('__KEY__', netcdfkey)
+    sti = sti.replace('__FILE__', 'exp_' + netcdfkey)
+    return sti
+
+def datakeys_command(clusteruser=CLUSTER_USER,
+                     clusterip=CLUSTER_IP,
+                     clim1_dd=CLUSTER_DATA_DIR,
+                     clim1_bd=CLUSTER_BUILD_DIR):
+    # make export-file NETCDFKEY=yourkeysearch NETCDFOUT=nameofnetcdfoutfile
+    sti = 'ssh  __UNAME__@__HOST__ -T "export CLIMATE1_CASSANDRA_DATA_DIR=__DATA_DIR__ && export CLIMATE1_BUILD_DIR=__BUILD_DIR__ && cd __BUILD_DIR__/bde-climate-1 && make -s cassandra-get-datasets"'
+    sti = sti.replace('__UNAME__', clusteruser)
+    sti = sti.replace( '__HOST__', clusterip)
+    sti = sti.replace('__DATA_DIR__', clim1_dd)
+    sti = sti.replace('__BUILD_DIR__', clim1_bd)
+    return sti
+
+global ncfiles
+ncfiles = []
+def filestolist(x):
+    ncfiles.append(x.strip())
+
+
+def netcdf_files():
+    global ncfiles 
+    ncfiles = []
+    execu('ls *.nc', fn=filestolist)
+    return ncfiles[:-1]
+
+
+global dataset_keys
+dataset_keys = []
+def populate_cassandra_keys_list(l):
+    dataset_keys.append(l.strip())
+
+def get_cassandra_data_keys():
+    global dataset_keys
+    dataset_keys = []
+    execu(datakeys_command(), fn=populate_cassandra_keys_list)
+    return dataset_keys[:-1]
+
+
+def update_export_HTML(x):
+    tx_export.value = x.strip()
+
+def export_clicked(b):
+    execu(export_command(netcdfkey=dd_export.value), fn=update_export_HTML)
+
+def display_export_form():
+    # Find available datasets/keys
+    # Display dropdown list
+    # Export button
+    global dd_export
+    global bt_export
+    global tx_export
+    global datakeys
+
+    datakeys = get_cassandra_data_keys()
+
+    l = widgets.HTML(
+        value = '<span style="color:#fff;">................................................... </span> '
+    )
+    dd_export = widgets.Dropdown(
+        options=get_cassandra_data_keys(),
+        description='Available keys:',
+    )
+    bt_export = widgets.Button(description="Export")
+    tx_export = widgets.Textarea()
+    container = widgets.HBox(children=[dd_export, l, bt_export, tx_export])
+
+    bt_export.on_click(export_clicked)
+    display(container)
+
+def display_ingest_form():
+    global w
+    global b
+    global txarea
+
+    netcdf_files()
+
+    l = widgets.HTML(
+        value = '<span style="color:#fff;">................................................... </span> '
+    )
+    w = widgets.Dropdown(
+        options=ncfiles,
+        description='Choose file:',
+    )
+    b = widgets.Button(description='Ingest')
+    txarea = widgets.Textarea()
+    container = widgets.HBox(children=[w, l, b, txarea])
+
+    b.on_click(ingest_clicked)
+    display(container)
     
+def ingest_clicked(b):
+    ingest(filename=w.value)
     
 def export(filename):
     ''' make export-file NETCDFKEY=yourkeysearch NETCDFOUT=nameofnetcdfoutfile '''
@@ -73,42 +220,21 @@ def view_data(data=0):
     print('a')
     pass
 
-def hive_bash(clusteruser='stathis', clusterip='172.17.20.106', clim1_bd='/home/stathis/Downloads', clim1_dd='/home/stathis/bde-climate-1', command='ls /home'):
-    sti = 'ssh __UNAME__@__HOST__ -t "export CLIMATE1_CASSANDRA_DATA_DIR=__DATA_DIR__ && export CLIMATE1_BUILD_DIR=__BUILD_DIR__ && cd __BUILD_DIR__ && docker exec -it hive __COMMND__"' 
+def hive_command(clusteruser='stathis', clusterip='172.17.20.106', clim1_bd='/home/stathis/Downloads', clim1_dd='/home/stathis/bde-climate-1', command='ls /home'):
+    sti = 'ssh  __UNAME__@__HOST__ -T "export CLIMATE1_CASSANDRA_DATA_DIR=__DATA_DIR__ && export CLIMATE1_BUILD_DIR=__BUILD_DIR__ && cd __BUILD_DIR__ && docker exec -i hive __COMMND__"' 
     sti = sti.replace('__UNAME__', clusteruser)
     sti = sti.replace( '__HOST__', clusterip)
     sti = sti.replace('__DATA_DIR__', clim1_dd)
     sti = sti.replace('__BUILD_DIR__', clim1_bd)
     sti = sti.replace('__COMMND__', command)
     print(sti)
-    print(exec_bash(sti))
-
+    return sti
 
 def exec_bash(command):
     return os.popen(command).read()
     #p = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
     #output = p.stdout.read()
     #return output
-
-import subprocess
-import sys
-from subprocess import Popen, PIPE, STDOUT
-
-def execu(command, pattern=None, fn=sys.stdout.write):
-    """
-    pattern is not really useful;
-    fn is being called multiple times until the command has completed
-    """
-    p = subprocess.Popen(str(command), shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-    while True:
-        l = p.stdout.readline()
-        if pattern == None:
-            fn(l)
-        else:
-            if str(l).find(pattern) > 0:
-                fn(l)
-        if p.poll() != None:
-            break
 
 # Test:
 # execu('ls / | grep "te"')
