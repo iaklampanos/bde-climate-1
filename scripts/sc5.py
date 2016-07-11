6682d3c57f154db3e3824cc2a9b3079878d69f24
grstathis <grstathis@gmail.com>
2016-07-11 17:42:33 +0300

    jquery for prov display

--- scripts/sc5.py
+++ scripts/sc5.py
@@ -14,14 +14,20 @@ import graphviz as gv
 import json
 import networkx as nx
 
+from datetime import datetime
+
 HIVE='localhost:10000'
 PILOT_PATH = '/home/stathis/bde-climate-1'
 NC_FILENAME = '/home/stathis/sc5/rsdscs_Amon_HadGEM2-ES_rcp26_r2i1p1_200512-203011.nc'
 
 CLUSTER_USER = 'stathis'
-CLUSTER_IP = '172.17.20.106'
+#CLUSTER_IP = '172.17.20.114'
+CLUSTER_IP = 'athina'
 CLUSTER_DATA_DIR = '/home/stathis/Downloads'
 CLUSTER_BUILD_DIR = '/home/stathis/Develop'
+
+def get_stamp(msg=''):
+    return msg + ' ' + str(datetime.now())
 
 def run_shell_command(s):
     c = s.split('|')
@@ -52,12 +58,14 @@ def _create_user_structure():
     run_shell_command("make -C /home/stathis/bde-climate-1 create-structure")
     
 def updateIngestHTML(x):
-    txarea.value = x
+    global txarea
+    txarea.value += x
 
 def ingest(filename):
     ''' make ingest-file NETCDFFILE=yourfilewithfullpath '''
-    # print 'Ingesting', filename
-    s = execu(ingest_command(netcdffile=filename))#, pattern="LOAD", fn=updateIngestHTML)
+    txarea.value += get_stamp('Starting ingest') + '\n'
+    s = execu(ingest_command(netcdffile=filename), pattern=None, fn=updateIngestHTML)
+    txarea.value += get_stamp('Finished ingest')
 
 def execu(command, pattern=None, fn=sys.stdout.write):
     """
@@ -162,10 +170,12 @@ def get_cassandra_data_keys():
 
 
 def update_export_HTML(x):
-    tx_export.value = x.strip()
+    tx_export.value += x.strip() + '<br/>'
 
 def export_clicked(b):
+    tx_export.value += get_stamp('Starting export') + '<br/>'
     execu(export_command(netcdfkey=dd_export.value), fn=update_export_HTML)
+    tx_export.value += get_stamp('Finished export')
     
 def display_export_form():
     # Find available datasets/keys
@@ -186,49 +196,59 @@ def display_export_form():
         description='Available keys:',
     )
     bt_export = widgets.Button(description="Export")
-    tx_export = widgets.Textarea()
-    container = widgets.HBox(children=[dd_export, l, bt_export, tx_export])
+    #tx_export = widgets.Textarea(height=3)
+    tx_export = widgets.HTML()
+    container = widgets.HBox(children=[dd_export, l, bt_export])
 
     bt_export.on_click(export_clicked)
     display(container)
+    display(tx_export)
 
 def update_prov(l):
     global provlist
     l = l.strip()
     if l == '': return
     provlist.append(json.loads(l))
-    # print '> ' + str(provlist)
-
-    # if l == '': return
-    # jsonl = json.loads(l)
-    
-    # g1.node(jsonl['id'])
-    # g1.node('B')
-    # g1.edge('A', 'B')
-    # print(g1.source)
-    # filename = g1.render(filename='img/g1',)
-    # print filename
 
 import matplotlib.image as mpimg
+
+from IPython.display import Image, display
+from IPython.core.display import HTML
+
+def get_short_id(id):
+    return id[:6]+'...'
+
 def prov_clicked(b):
     global provlist
+    global html_prov
+
+    html_prov.value = 'Drawing data lineage... <br/>' + html_prov.value
     provlist = []
     execu(prov_command(netcdfkey=dd_prov.value), fn=update_prov)
 
-    g1 = gv.Graph(format='png')
+    g1 = gv.Digraph(format='png')
     for i in provlist:
-        g1.node(i['id'])
+        # print i
+        g1.node(get_short_id(i['id']), '\n'.join(i['paths']))
         if i['parentid'] is not None:
-            g1.node(i['parentid'])
-            g1.edge(i['parentid'], i['id'])
+            g1.node(get_short_id(i['parentid']))
+            lbl = ''
+            if 'downscaling' in i and 'agentname' in i['downscaling'][0]:
+                lbl = i['downscaling'][0]['agentname'] + '\n' + i['downscaling'][0]['et']
+            g1.edge( get_short_id(i['parentid']), get_short_id(i['id']), label=lbl )
         if i['bparentid'] is not None:
-            g1.node(i['bparentid'])
-            g1.edge(i['bparentid'], i['id'])
+            g1.node(get_short_id(i['bparentid']))
+            lbl = ''
+            if 'downscaling' in i and 'agentname' in i['downscaling'][0]:
+                lbl = i['downscaling'][0]['agentname'] + '\n' + i['downscaling'][0]['et']
+            g1.edge( get_short_id(i['bparentid']), get_short_id(i['id']), label=lbl )
     # print g1.source
-    g1.render(filename='img/g1',)
 
-    global html_prov
-    html_prov.value = '<img src="img/g1.png"/>'
+    g1.render(filename='img/g1')
+
+    #html_prov.value = '<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" /> <meta http-equiv="Pragma" content="no-cache" /> <meta http-equiv="Expires" content="0" />' 
+    html_prov.value = '<div><img id="myimg" src=""></div>'
+    html_prov.value += ' <script> d = new Date(); $("#myimg").attr("src", "img/g1.png?"+d.getTime()); console.log(d.getTime());</script>'
 
 def wrf_clicked(b):
     global dd_reg_wrf
@@ -242,12 +262,18 @@ def wrf_clicked(b):
         reg = 'd01'
     elif dd_reg_wrf.value == 'Greece':
         reg = 'd02'
+    elif dd_reg_wrf.value == 'Europe-->Greece':
+        reg = 'd01d02'
     else:
         pass
 
     stdate = dd_st_wrf.value.replace('-', '')
     dur = dd_dur_wrf.value
-    execu(wrf_command(region=reg, startdate=stdate, duration=dur))
+    if reg == 'd01d02':
+        #print 'run nesting'
+        execu(wrf_command_nest(region=reg, startdate=stdate, duration=dur))
+    else:
+        execu(wrf_command(region=reg, startdate=stdate, duration=dur))
     
 def display_wrf_form():
     global dd_reg_wrf
@@ -261,7 +287,7 @@ def display_wrf_form():
     )
 
     dd_reg_wrf = widgets.Dropdown(
-        options=["Europe", "Greece"],
+        options=["Europe", "Greece", "Europe-->Greece"],
         value="Europe",
         description='Available regions:',
     )
@@ -288,10 +314,10 @@ def display_prov_form():
     global dd_prov
     global bt_prov
     global html_prov
-    global datakeys
 
-    datakeys = get_cassandra_data_keys()
-
+    #global datakeys
+    #datakeys = get_cassandra_data_keys()
+    
     l = widgets.HTML(
         value = '<span style="color:#fff;">................................................... </span> '
     )
@@ -323,11 +349,12 @@ def display_ingest_form():
         description='Choose file:',
     )
     b = widgets.Button(description='Ingest')
-    txarea = widgets.Textarea()
-    container = widgets.HBox(children=[w, l, b, txarea])
+    txarea = widgets.Textarea(height=3)
+    container = widgets.HBox(children=[w, l, b])
 
     b.on_click(ingest_clicked)
     display(container)
+    display(txarea)
     
 def ingest_clicked(b):
     ingest(filename=w.value)
@@ -356,6 +383,29 @@ def wrf_command(clusteruser=CLUSTER_USER,
     sti = sti.replace('__REGION__', region)
     sti = sti.replace('__CUSER__', curr_user)
     return sti
+
+def wrf_command_nest(clusteruser=CLUSTER_USER,
+                clusterip=CLUSTER_IP,
+                clim1_dd=CLUSTER_DATA_DIR,
+                clim1_bd=CLUSTER_BUILD_DIR,
+                region ='R1',
+                startdate = '20070101',
+                duration = 6):
+    # make export-file NETCDFKEY=yourkeysearch NETCDFOUT=nameofnetcdfoutfile
+    curr_user = os.environ['USER']
+    sti = 'ssh  __UNAME__@__HOST__ -T "export CLIMATE1_CASSANDRA_DATA_DIR=__DATA_DIR__ && export CLIMATE1_BUILD_DIR=__BUILD_DIR__ && cd __BUILD_DIR__/bde-climate-1 && make -s run-wrf-nest RSTARTDT=__STARTDATE__ RDURATION=__RDURATION__ REG=__REGION__ CUSER=__CUSER__"'
+    sti = sti.replace('__UNAME__', clusteruser)
+    sti = sti.replace( '__HOST__', clusterip)
+    sti = sti.replace('__DATA_DIR__', clim1_dd)
+    sti = sti.replace('__BUILD_DIR__', clim1_bd)
+    sti = sti.replace('__STARTDATE__', startdate)
+    sti = sti.replace('__RDURATION__', duration)
+    sti = sti.replace('__REGION__', region)
+    sti = sti.replace('__CUSER__', curr_user)
+    return sti
+
+
+
 
 def _run_wrf():
     '''make run-wrf RSTARTDT=StartDateOfModel RDURATION=DurationOfModelInHours REG=<d01|d02|d03> '''
